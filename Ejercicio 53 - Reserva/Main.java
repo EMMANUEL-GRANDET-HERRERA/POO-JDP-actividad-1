5ba213be4bb43df5994d53ba406fa383f99bcd7a
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 53 - Reserva

---
+++ Ejercicio 53 - Reserva/Main.java
@@ -0,0 +1,7 @@
+package ejercicio53;
+
+public class Main {
+ public static void main(String[] args) {
+  Reserva r=new Reserva(); r.confirmar(); System.out.println(r.activa); r.cancelar();
+ }
+}
---
+++ Ejercicio 53 - Reserva/Reserva.java
@@ -0,0 +1,3 @@
+package ejercicio53;
+
+public class Reserva { String nombreCliente,fecha; boolean activa; void confirmar(){activa=true;} void cancelar(){activa=false;} }
