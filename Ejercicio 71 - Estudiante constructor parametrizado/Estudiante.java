3e2ec5c367a55b7c052f494db3405b2478f703fa
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:37 -0500

    Ejercicio 71 - Estudiante constructor parametrizado

---
+++ Ejercicio 71 - Estudiante constructor parametrizado/Estudiante.java
@@ -0,0 +1,3 @@
+package ejercicio71;
+
+class Estudiante { String nombre,codigo; int semestre; Estudiante(String n,String c,int s){nombre=n;codigo=c;semestre=s;} }
---
+++ Ejercicio 71 - Estudiante constructor parametrizado/Main.java
@@ -0,0 +1,7 @@
+package ejercicio71;
+
+public class Main {
+ public static void main(String[] args) {
+  Estudiante e=new Estudiante("Ana","001",3); System.out.println(e.nombre);
+ }
+}
