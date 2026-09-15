31ad940bf13a98b6178db77aa11bdc63bf28b94f
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 55 - Reloj

---
+++ Ejercicio 55 - Reloj/Main.java
@@ -0,0 +1,7 @@
+package ejercicio55;
+
+public class Main {
+ public static void main(String[] args) {
+  Reloj r=new Reloj(); r.hora=14; r.minuto=30; r.segundo=45; r.mostrarHora();
+ }
+}
---
+++ Ejercicio 55 - Reloj/Reloj.java
@@ -0,0 +1,3 @@
+package ejercicio55;
+
+public class Reloj { int hora,minuto,segundo; void mostrarHora(){System.out.printf("%02d:%02d:%02d%n",hora,minuto,segundo);} }
