39abe2e242566cab8ac0debbec2530be0b2974d2
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 31 - Construccion 31

---
+++ Ejercicio 31 - Construccion 31/Clase.java
@@ -0,0 +1,3 @@
+package ejercicio31;
+
+class Producto { String nombre; double precio; int stock; Producto(String n,double p,int s){nombre=n;precio=p;stock=s;} static Producto crearProductoBasico(String n,double p){return new Producto(n,p,0);} }
---
+++ Ejercicio 31 - Construccion 31/Main.java
@@ -0,0 +1,7 @@
+package ejercicio31;
+
+public class Main {
+ public static void main(String[] args) {
+  Producto p=Producto.crearProductoBasico("Cuaderno",5000); System.out.println(p.nombre);
+ }
+}
