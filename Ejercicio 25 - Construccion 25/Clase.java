990921c9d698dc43876fafcdd8cf131b90e25e7c
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 25 - Construccion 25

---
+++ Ejercicio 25 - Construccion 25/Clase.java
@@ -0,0 +1,3 @@
+package ejercicio25;
+
+class Libro { String titulo,autor; int paginas; Libro(){} Libro(String t,String a,int p){titulo=t;autor=a;paginas=p;} }
---
+++ Ejercicio 25 - Construccion 25/Main.java
@@ -0,0 +1,7 @@
+package ejercicio25;
+
+public class Main {
+ public static void main(String[] args) {
+  Libro a=new Libro(); Libro b=new Libro("1984","George Orwell",328); System.out.println(b.titulo);
+ }
+}
