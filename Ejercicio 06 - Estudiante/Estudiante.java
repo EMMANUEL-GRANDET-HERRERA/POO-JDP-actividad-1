7b0395e85fdb15ade8f47dd27f3768ce29c05914
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 06 - Estudiante

---
+++ Ejercicio 06 - Estudiante/Estudiante.java
@@ -0,0 +1,3 @@
+package ejercicio06;
+
+public class Estudiante { String nombre; String codigo; int semestre; void mostrarInfo(){System.out.println(nombre+" - "+codigo+" - "+semestre);} }
---
+++ Ejercicio 06 - Estudiante/Main.java
@@ -0,0 +1,7 @@
+package ejercicio06;
+
+public class Main {
+ public static void main(String[] args) {
+  Estudiante e=new Estudiante(); e.nombre="Ana"; e.codigo="001"; e.semestre=2; e.mostrarInfo();
+ }
+}
