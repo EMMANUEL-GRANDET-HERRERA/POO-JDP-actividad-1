493f60bdf215fe8d7e8d444cc7647ac9da3d910b
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 12 - Vehiculo

---
+++ Ejercicio 12 - Vehiculo/Main.java
@@ -0,0 +1,7 @@
+package ejercicio12;
+
+public class Main {
+ public static void main(String[] args) {
+  Vehiculo v=new Vehiculo(); v.velocidadActual=5; v.frenar(); System.out.println(v.velocidadActual);
+ }
+}
---
+++ Ejercicio 12 - Vehiculo/Vehiculo.java
@@ -0,0 +1,3 @@
+package ejercicio12;
+
+public class Vehiculo { int velocidadActual; void frenar(){velocidadActual=Math.max(0,velocidadActual-10);} }
