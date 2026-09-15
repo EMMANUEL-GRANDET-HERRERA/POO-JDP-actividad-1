06945f12a18ba922a357c61ccedd1ea8e367491c
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 32 - Construccion 32

---
+++ Ejercicio 32 - Construccion 32/Main.java
@@ -0,0 +1,7 @@
+package ejercicio32;
+
+public class Main {
+ public static void main(String[] args) {
+  Producto a=Producto.crearProductoBasico("Cuaderno",5000); Producto b=Producto.crearProductoBasico("Lapicero",2000); System.out.println(a.nombre+" / "+b.nombre);
+ }
+}
---
+++ Ejercicio 32 - Construccion 32/README.md
@@ -0,0 +1,1 @@
+Dos productos mediante fábrica.
