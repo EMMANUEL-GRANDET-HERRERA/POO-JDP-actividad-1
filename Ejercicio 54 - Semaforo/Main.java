e0d2669c497686ff49dcb5c1efd7289e817adfbd
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 54 - Semaforo

---
+++ Ejercicio 54 - Semaforo/Main.java
@@ -0,0 +1,7 @@
+package ejercicio54;
+
+public class Main {
+ public static void main(String[] args) {
+  Semaforo s=new Semaforo(); s.color="rojo"; s.cambiar(); System.out.println(s.color); s.cambiar(); System.out.println(s.color); s.cambiar(); System.out.println(s.color);
+ }
+}
---
+++ Ejercicio 54 - Semaforo/Semaforo.java
@@ -0,0 +1,3 @@
+package ejercicio54;
+
+public class Semaforo { String color; void cambiar(){if(color.equals("rojo"))color="amarillo";else if(color.equals("amarillo"))color="verde";else color="rojo";} }
