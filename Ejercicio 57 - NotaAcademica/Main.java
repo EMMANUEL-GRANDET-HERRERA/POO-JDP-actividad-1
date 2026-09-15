3dfbe398e95c3779d330770279ef57c3e4a2e665
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 57 - NotaAcademica

---
+++ Ejercicio 57 - NotaAcademica/Main.java
@@ -0,0 +1,7 @@
+package ejercicio57;
+
+public class Main {
+ public static void main(String[] args) {
+  NotaAcademica n=new NotaAcademica(); n.nota1=4; n.nota2=4.5; n.nota3=3.5; System.out.println(n.definitiva());
+ }
+}
---
+++ Ejercicio 57 - NotaAcademica/NotaAcademica.java
@@ -0,0 +1,3 @@
+package ejercicio57;
+
+public class NotaAcademica { String asignatura; double nota1,nota2,nota3; double definitiva(){return (nota1+nota2+nota3)/3;} }
