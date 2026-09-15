4ba33b1ff76e8e19058ae3b00d8a78be13c0d5e2
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 67 - ActividadDiaria

---
+++ Ejercicio 67 - ActividadDiaria/ActividadDiaria.java
@@ -0,0 +1,3 @@
+package ejercicio67;
+
+class ActividadDiaria { String nombre,horario; void mostrarInfo(){System.out.println(nombre+" - "+horario);} }
---
+++ Ejercicio 67 - ActividadDiaria/Main.java
@@ -0,0 +1,7 @@
+package ejercicio67;
+
+public class Main {
+ public static void main(String[] args) {
+  ActividadDiaria a=new ActividadDiaria(); a.nombre="Estudiar"; a.horario="18:00"; a.mostrarInfo();
+ }
+}
