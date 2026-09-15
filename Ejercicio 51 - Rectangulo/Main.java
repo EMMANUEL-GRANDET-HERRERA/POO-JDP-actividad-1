144ebbdb0db8766a975cbb69e5e4bd28924456eb
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 51 - Rectangulo

---
+++ Ejercicio 51 - Rectangulo/Main.java
@@ -0,0 +1,7 @@
+package ejercicio51;
+
+public class Main {
+ public static void main(String[] args) {
+  Rectangulo r=new Rectangulo(); r.base=5; r.altura=3; System.out.println(r.area()); System.out.println(r.perimetro());
+ }
+}
---
+++ Ejercicio 51 - Rectangulo/Rectangulo.java
@@ -0,0 +1,3 @@
+package ejercicio51;
+
+public class Rectangulo { double base,altura; double area(){return base*altura;} double perimetro(){return 2*(base+altura);} }
