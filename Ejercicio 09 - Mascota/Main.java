6f2fd4ac2aa863449d267a510887761616dc69d6
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 09 - Mascota

---
+++ Ejercicio 09 - Mascota/Main.java
@@ -0,0 +1,7 @@
+package ejercicio09;
+
+public class Main {
+ public static void main(String[] args) {
+  Mascota m=new Mascota(); m.nombre="Luna"; m.especie="Perro"; m.edad=3; m.mostrarMascota(); m.edad=4; m.mostrarMascota(); m.edad=5; m.mostrarMascota();
+ }
+}
---
+++ Ejercicio 09 - Mascota/Mascota.java
@@ -0,0 +1,3 @@
+package ejercicio09;
+
+public class Mascota { String nombre; String especie; int edad; void mostrarMascota(){System.out.println(nombre+" - "+especie+" - "+edad+" años");} }
