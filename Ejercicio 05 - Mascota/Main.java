9f62dc6a0be6de9accf41745ea6cc4f080213d6c
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 05 - Mascota

---
+++ Ejercicio 05 - Mascota/Main.java
@@ -0,0 +1,13 @@
+package ejercicio05;
+
+public class Main {
+    public static void main(String[] args) {
+        Mascota mascota = new Mascota();
+
+        mascota.nombre = "Luna";
+        mascota.especie = "Perro";
+        mascota.edad = 3;
+
+        mascota.mostrarMascota();
+    }
+}
