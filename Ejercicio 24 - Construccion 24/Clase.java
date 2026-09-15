934629f7f2f3b4d98e83e4fccc564526f26c4057
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 24 - Construccion 24

---
+++ Ejercicio 24 - Construccion 24/Clase.java
@@ -0,0 +1,3 @@
+package ejercicio24;
+
+class Libro { String titulo,autor; int paginas; Libro(String t,String a,int p){titulo=t;autor=a;paginas=p;} }
---
+++ Ejercicio 24 - Construccion 24/Main.java
@@ -0,0 +1,7 @@
+package ejercicio24;
+
+public class Main {
+ public static void main(String[] args) {
+  Libro l=new Libro("Rayuela","Julio Cortázar",600); System.out.println(l.titulo);
+ }
+}
