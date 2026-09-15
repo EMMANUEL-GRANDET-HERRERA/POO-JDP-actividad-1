465c24f93fa80723632567c2e8d0aa85344dd8c7
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 60 - Avion

---
+++ Ejercicio 60 - Avion/Avion.java
@@ -0,0 +1,3 @@
+package ejercicio60;
+
+public class Avion { String aerolinea,destino; int pasajeros; void abordar(int cantidad){if(cantidad>0)pasajeros+=cantidad;} }
---
+++ Ejercicio 60 - Avion/Main.java
@@ -0,0 +1,7 @@
+package ejercicio60;
+
+public class Main {
+ public static void main(String[] args) {
+  Avion a=new Avion(); a.destino="Cartagena"; a.abordar(80); System.out.println(a.pasajeros);
+ }
+}
