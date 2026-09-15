078aa54fce8b3210e2b67dcfd25ec7812f6dac70
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 28 - Construccion 28

---
+++ Ejercicio 28 - Construccion 28/Main.java
@@ -0,0 +1,7 @@
+package ejercicio28;
+
+public class Main {
+ public static void main(String[] args) {
+  CuentaBancaria a=new CuentaBancaria("001","Ana",100000); CuentaBancaria b=new CuentaBancaria("002","Luis",250000); CuentaBancaria c=new CuentaBancaria("003","Marta",50000); System.out.println(a.saldo+" "+b.saldo+" "+c.saldo);
+ }
+}
