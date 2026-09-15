d70fe68766195f9eb1cf011dfdecce9c95893283
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 26 - Construccion 26

---
+++ Ejercicio 26 - Construccion 26/Clase.java
@@ -0,0 +1,3 @@
+package ejercicio26;
+
+class Producto { String nombre; double precio; int stock; Producto(){} Producto(String n,double p,int s){nombre=n;precio=p;stock=s;} }
---
+++ Ejercicio 26 - Construccion 26/Main.java
@@ -0,0 +1,7 @@
+package ejercicio26;
+
+public class Main {
+ public static void main(String[] args) {
+  Producto a=new Producto(); Producto b=new Producto("Leche",3800,15); System.out.println(b.nombre);
+ }
+}
