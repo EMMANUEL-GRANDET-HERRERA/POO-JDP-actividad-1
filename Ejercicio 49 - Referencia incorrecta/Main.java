421bfd5763478e73175668a1300ac7697208f0a4
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 49 - Referencia incorrecta

---
+++ Ejercicio 49 - Referencia incorrecta/Main.java
@@ -0,0 +1,4 @@
+package ejercicio49;
+
+class Estudiante { String nombre; }
+public class Main { public static void main(String[] args){ Estudiante e1=new Estudiante(); e1.nombre="Ana"; System.out.println(e1.nombre); } }
