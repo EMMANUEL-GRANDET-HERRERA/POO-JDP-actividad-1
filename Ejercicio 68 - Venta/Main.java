8e04fbb460b881c2fca3db84e7f01f229ceb5e6c
EMMANUEL-GRANDET-HERRERA <egrandeth@unicartagena.edu.co>
2026-09-15 12:21:36 -0500

    Ejercicio 68 - Venta

---
+++ Ejercicio 68 - Venta/Main.java
@@ -0,0 +1,7 @@
+package ejercicio68;
+
+public class Main {
+ public static void main(String[] args) {
+  Venta v=new Venta(); v.producto="Producto A"; v.valor=50000; v.mostrarInfo();
+ }
+}
---
+++ Ejercicio 68 - Venta/Venta.java
@@ -0,0 +1,3 @@
+package ejercicio68;
+
+class Venta { String producto; double valor; void mostrarInfo(){System.out.println(producto+" "+valor);} }
